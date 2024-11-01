08375843fdde0ce8c47518bc82626167e3f4a501
Kayla Lange <lan17019@byui.edu>
2024-11-01 16:53:18 -0600

    adjusted font weights and changed color scheme

--- scripts/homepage.js
+++ scripts/homepage.js
@@ -4,7 +4,6 @@ const hambutton = document.querySelector('#menu');
 function toggleMenu() {
     mainnav.classList.toggle('show');
     hambutton.classList.toggle('show');
-    h1.classList.toggle('show');
 }
 
 hambutton.addEventListener('click', toggleMenu);
