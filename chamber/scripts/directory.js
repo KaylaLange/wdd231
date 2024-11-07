cf9095e9caf57b483644657d78a982cc48626022
Kayla Lange <lan17019@byui.edu>
2024-11-07 13:14:08 -0700

    saving progress: added javascript and moved normalize.css to my chamber styles folder

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -1,28 +1,45 @@
 document.addEventListener("DOMContentLoaded", () => {
     fetchMemberData();
-});
 
-async function fetchMemberData() {
-    try {
-        const response = await fetch('../data/members.json');
-        const data = await response.json();
-        displayMembers(data.members);
-    } catch (error) {
-        console.error('Error fetching member data:', error);
+    // to fetch json data and display on page
+    async function fetchMemberData() {
+        try {
+            const response = await fetch('data/members.json'); 
+            const data = await response.json();
+            displayMembers(data.members);
+        } catch (error) {
+            console.error('Error fetching member data:', error);
+        }
     }
-}
 
-function displayMembers(members) {
-    const membersContainer = document.getElementById('members');
-    members.forEach(member => {
-        const memberDiv = document.createElement('div');
-        memberDiv.classList.add('member');
-        memberDiv.innerHTML = `
-        <h2>${member.business_name}</h2>
-        <p>${member.address}</p>
-        <p>${member.phone_number}</p>
-        <p><a href="${member.website}" target="_blank">${member.website}</a></p>`;
+    function displayMembers(members) {
+        const membersContainer = document.getElementById('members');
+        members.forEach(member => {
+            const memberDiv = document.createElement('div');
+            memberDiv.classList.add('member');
+            memberDiv.innerHTML = `
+            <h2>${member.business_name}</h2>
+            <p>${member.address}</p>
+            <p>${member.phone_number}</p>
+            <p><a href="${member.website}" target="_blank">${member.website}</a></p>`;
 
-        membersContainer.appendChild(memberDiv);
-    });
-}
+            membersContainer.appendChild(memberDiv);
+        });
+    }
+
+    // grid and list button 
+    const gridButton = document.querySelector('#grid');
+    const listButton = document.querySelector('#list');
+    const display = document.querySelector('members');
+
+    const toggleView = (view) => {
+        display.classList.remove('grid', 'list');
+        display.classList.add(view);
+    };
+
+    toggleView('grid');
+
+    gridButton.addEventListener('click', () => toggleView('grid'));
+    listButton.addEventListener('click', () => toggleView('list'));
+
+});
