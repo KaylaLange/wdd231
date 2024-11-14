57586e8eed30d4324c4fe4948f97cab57b598356
Kayla Lange <lan17019@byui.edu>
2024-11-14 14:32:45 -0700

    cleaning up html errors

--- scripts/homepage.js
+++ scripts/homepage.js
@@ -31,10 +31,12 @@ const url = 'https://api.openweathermap.org/data/2.5/weather?lat=40.38&lon=-105.
 
 async function apiFetch(url) {
     try {
+        console.log("Fetching data from URL:", url);
         const response = await fetch(url);
         if (response.ok) {
             const data = await response.json();
-            console.log(data);
+            console.log("Fetched data:", data);
+            // console.log(data);
             displayResults(data);
         } else {
             throw Error(await response.text());
@@ -45,14 +47,18 @@ async function apiFetch(url) {
 }
 
 function displayResults(data) {
+    console.log("Displaying results:", data);
     currentTemp.innerHTML = `${data.main.temp}&deg;F`;
     feelsLike.innerHTML = `${data.main.feels_like}&deg;F`;
     windSpeed.innerHTML = `${data.wind.speed}mph`;
     windGusts.innerHTML = `${data.wind.gust}mph`;
     const iconsrc = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+    console.log("Icon src:", iconsrc);
     let desc = data.weather[0].description;
-    weatherIcon.setAttribute('src',iconsrc);
-    weatherIcon.setAttribute('alt',desc);
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('alt', desc);
+    console.log("Image src attribute set to:", weatherIcon.getAttribute('src'));
+    console.log("Image alt attribute set to:", weatherIcon.getAttribute('alt'));
     captionDesc.textContent = `${desc}`;
 }
 apiFetch(url);
