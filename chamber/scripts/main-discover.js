8cfe94cc08c6f8fb26031e1dc7cc04deb9d24101
Kayla Lange <lan17019@byui.edu>
2024-12-03 22:08:44 -0700

    working on css for discover page

---
+++ chamber/scripts/main-discover.js
@@ -0,0 +1,7 @@
+import { initHamburger } from "./hamburger.js";
+import { updateDates } from "./getdate.js";
+
+document.addEventListener('DOMContentLoaded', () => {
+    initHamburger();
+    updateDates();
+});
