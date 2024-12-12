4bc614d8ffa72fcbb02fb55de055fee7b1b87ddf
Kayla Lange <lan17019@byui.edu>
2024-12-12 11:33:46 -0700

    created about us page and applicable css

--- final-project-website/scripts/main-about.js
+++ final-project-website/scripts/main-about.js
@@ -1,9 +1,7 @@
 import { initHamburger } from "./hamburger.js";
 import { updateDates } from "./getdate.js";
-import { show } from "./thankyou.js";
 
 document.addEventListener('DOMContentLoaded', () => {
     initHamburger();
     updateDates();
-    show();
 });
